ce99d297c04101dab98ef7da2f913ff702c9bbfb
SattaSam <samuel.lemasson@gmail.com>
2026-07-20 12:35:43 +0200

    V14
    fix ergo paysage zone invulnérabilité, texte verouillé

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'la-boule-bleue-v13-final';
+const CACHE = 'la-boule-bleue-v14-landscape';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icons/icon-192.png', './icons/icon-512.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
